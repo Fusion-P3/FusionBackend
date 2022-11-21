ccf3ce1435f41b059d93097e5eb0210837190462
Jacob Allen <jallen1887@gmail.com>
2022-11-21 16:37:06 -0500

    Fix the warnings

--- ECommerce.Models/Product.cs
+++ ECommerce.Models/Product.cs
@@ -9,11 +9,11 @@ namespace ECommerce.Models
     public class Product
     {
         public int id { get; set; }
-        public string name { get; set; }
+        public string? name { get; set; }
         public int quantity { get; set; }
         public decimal price { get; set; }
-        public string description { get; set; }
-        public string image { get; set; }
+        public string? description { get; set; }
+        public string? image { get; set; }
 
         public Product() { }
 
--- ECommerce.Models/User.cs
+++ ECommerce.Models/User.cs
@@ -2,10 +2,10 @@
 {
     public class User
     {
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
 
         public User() { }
 
