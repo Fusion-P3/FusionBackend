3dd5012d2a4cf3b7345915c26fe162078097d164
Gregory Sabado <gregory793@revature.net>
2022-11-28 13:41:55 -0800

    fleshed out get product by id test

--- ECommerce.Tests/ProductTests.cs
+++ ECommerce.Tests/ProductTests.cs
@@ -74,25 +74,53 @@ public class ProductTests
         var service = CreateProductService();
         Guid guid1 = Guid.NewGuid();
         Guid guid2 = Guid.NewGuid();
-        string name = "test";
+        string name1 = "test";
+        string name2 = "test2";
         int quantity = 10;
+        int quantity2 = 12;
         int price = 10;
+        int price2 = 10;
         string description = "test description";
+        string description2 = "test description2";
         string image = "test image";
-        ECommerce.Models.Product testProd1 = new(guid1, name, quantity, price, description, image);
-        ECommerce.Models.Product testProd2 = new(guid2, name, quantity, price, description, image);
+        string image2 = "test image2";
+        ECommerce.Models.Product testProd1 = new(guid1, name1, quantity, price, description, image);
+        ECommerce.Models.Product testProd2 = new(guid2, name2, quantity2, price2, description2, image2);
 
         products.Add(testProd1);
         products.Add(testProd2);
 
+        // two different products
         ECommerce.Models.Product product1 = await service.GetProductByIdAsync(guid1);
         ECommerce.Models.Product product2 = await service.GetProductByIdAsync(guid2);
 
+        //invalid
+        ECommerce.Models.Product product3 = await service.GetProductByIdAsync(Guid.Empty);
+
         Assert.NotNull(product1);
         Assert.NotNull(product2);
+        Assert.NotNull(product3);
 
         Assert.Equal(testProd1.id, product1.id);
+        Assert.Equal(testProd1.name, product1.name);
+        Assert.Equal(testProd1.quantity, product1.quantity);
+        Assert.Equal(testProd1.price, product1.price);
+        Assert.Equal(testProd1.description, product1.description);
+        Assert.Equal(testProd1.image, product1.image);
+
         Assert.Equal(testProd2.id, product2.id);
+        Assert.Equal(testProd2.name, product2.name);
+        Assert.Equal(testProd2.quantity, product2.quantity);
+        Assert.Equal(testProd2.price, product2.price);
+        Assert.Equal(testProd2.description, product2.description);
+        Assert.Equal(testProd2.image, product2.image);
+
+        Assert.Equal(product3.id, Guid.Empty);
+        Assert.Null(product3.name);
+        Assert.Null(product3.quantity);
+        Assert.Null(product3.price);
+        Assert.Null(product3.description);
+        Assert.Null(product3.image);
 
         products.Clear();
     }
