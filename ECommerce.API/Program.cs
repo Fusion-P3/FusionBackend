c43f53c820175713cad532e3a188902249f06140
Jacob Allen <jallen1887@gmail.com>
2022-12-05 17:51:12 -0500

    Yes I am commiting on main

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -15,8 +15,7 @@ builder.Services.AddCors(options =>
         {
             policy.AllowAnyOrigin()
                    .AllowAnyMethod()
-                   .AllowAnyHeader()
-                   .AllowCredentials();
+                   .AllowAnyHeader();
         });
 });
 
