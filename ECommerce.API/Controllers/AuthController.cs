e0bdd8a46fc4615ffb46319b982b9535a5f44d52
Jacob Allen <jallen1887@gmail.com>
2022-12-06 16:08:52 -0500

    get rid of warnings

--- ECommerce.API/Controllers/AuthController.cs
+++ ECommerce.API/Controllers/AuthController.cs
@@ -80,7 +80,7 @@ namespace ECommerce.API.Controllers
                 if (LoginAuth.problemsCompleted != newProbsCompleted)
                 {
                     Guid userId = LoginAuth.userId;
-                    await _inventoryService.UpdateInventoryItem(userId, Guid.Parse("33914a4d-5e85-48cf-a443-e70672eb07d0"), (newProbsCompleted - LoginAuth.problemsCompleted.Value) * 100);
+                    await _inventoryService.UpdateInventoryItem(userId, Guid.Parse("33914a4d-5e85-48cf-a443-e70672eb07d0"), (newProbsCompleted - LoginAuth.problemsCompleted!.Value) * 100);
                     await _service.UpdateProblemsCompleted(userId, newProbsCompleted);
                     LoginAuth.problemsCompleted = newProbsCompleted;
                 }
